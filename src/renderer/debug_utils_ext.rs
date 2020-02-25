ef375f64b8a02a5157feb1c6ac1196cfc880f94e
Brandon Pollack <brandonpollack23@gmail.com>
2020-02-24 16:23:09 -0800

    Still broken but almost done with debug messenger unit tests

--- src/renderer/debug_utils_ext.rs
+++ src/renderer/debug_utils_ext.rs
@@ -1,6 +1,11 @@
-use ash::{extensions::ext::DebugUtils, vk};
+use ash::{extensions::ext::DebugUtils, vk, Entry, Instance};
 use log::error;
-use std::ffi::{c_void, CStr};
+use std::{
+  ffi::{c_void, CStr},
+  mem::MaybeUninit,
+  pin::Pin,
+  sync::atomic::{AtomicUsize, Ordering},
+};
 
 // TODO to make unit tests etc work, we can pass this structure itself the
 // callback data, cast it as mut, and delegate to a do_debug_callback.
@@ -14,12 +19,36 @@ use std::ffi::{c_void, CStr};
 pub struct DebugUtilsAndMessenger {
   pub debug_utils: DebugUtils,
   pub messenger: vk::DebugUtilsMessengerEXT,
-
-  info_count: AtomicUsize,
-  warning_count: AtomicUsize,
-  error_count: AtomicUsize,
+  pub user_data: Pin<Box<DebugUserData>>,
 }
 impl DebugUtilsAndMessenger {
+  pub fn new(
+    entry: &Entry, instance: &Instance, severity_flags: vk::DebugUtilsMessageSeverityFlagsEXT,
+    type_flags: vk::DebugUtilsMessageTypeFlagsEXT,
+  ) -> Self {
+    let mut user_data_ptr = Box::pin(DebugUserData::new());
+
+    let debug_utils = DebugUtils::new(entry, instance);
+
+    let messenger_ci = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+      .message_severity(severity_flags)
+      .message_type(type_flags)
+      .pfn_user_callback(Some(Self::debug_callback))
+      .user_data(user_data_ptr.as_mut().get_mut() as *mut DebugUserData as *mut c_void)
+      .build();
+    let messenger = unsafe {
+      debug_utils
+        .create_debug_utils_messenger(&messenger_ci, None)
+        .expect("Could not create debug utils messenger")
+    };
+
+    DebugUtilsAndMessenger {
+      debug_utils,
+      messenger,
+      user_data: user_data_ptr,
+    }
+  }
+
   /// It is invariant in the vulkan renderer setup that p_user_data is of type
   /// DebugUtilsAndMessenger (see the [vulkan
   /// renderer](struct.VulkanRenderer.html) implementation.
@@ -28,18 +57,62 @@ impl DebugUtilsAndMessenger {
     message_types: vk::DebugUtilsMessageTypeFlagsEXT,
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT, p_user_data: *mut c_void,
   ) -> u32 {
-    error!(
-      "Validation Error! {}",
-      CStr::from_ptr((*p_callback_data).p_message as *const i8)
-        .to_str()
-        .unwrap()
-    );
+    let this: &mut DebugUserData = std::mem::transmute(p_user_data);
+    match message_severity {
+      vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => {
+        this.error_count.fetch_add(1, Ordering::Relaxed);
+      }
+      vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => {
+        this.warning_count.fetch_add(1, Ordering::Relaxed);
+      }
+      vk::DebugUtilsMessageSeverityFlagsEXT::INFO => {
+        this.info_count.fetch_add(1, Ordering::Relaxed);
+      }
+      _ => {}
+    }
+
+    match (message_severity, message_types) {
+      _ => {
+        error!(
+          "Validation Error! {}",
+          CStr::from_ptr((*p_callback_data).p_message as *const i8)
+            .to_str()
+            .unwrap()
+        );
+      }
+    }
 
     // Returning false indicates no error in callback.
     vk::FALSE
   }
 
-  fn handle_debug_callback(&mut self) -> u32 {
-
+  pub fn get_error_counts(&self) -> DebugUserDataCopy {
+    DebugUserDataCopy {
+      info_count: self.user_data.info_count.load(Ordering::Relaxed),
+      warning_count: self.user_data.warning_count.load(Ordering::Relaxed),
+      error_count: self.user_data.error_count.load(Ordering::Relaxed),
+    }
   }
+}
+
+#[repr(C)]
+pub struct DebugUserData {
+  info_count: AtomicUsize,
+  warning_count: AtomicUsize,
+  error_count: AtomicUsize,
+}
+impl DebugUserData {
+  fn new() -> Self {
+    Self {
+      info_count: AtomicUsize::new(0),
+      warning_count: AtomicUsize::new(0),
+      error_count: AtomicUsize::new(0),
+    }
+  }
+}
+
+pub struct DebugUserDataCopy {
+  pub info_count: usize,
+  pub warning_count: usize,
+  pub error_count: usize,
 }
