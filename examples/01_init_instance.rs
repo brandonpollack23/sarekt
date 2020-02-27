9b079043e0c9feee6f7aa266dfd042feb10ca4d0
Brandon Pollack <brandonpollack23@gmail.com>
2020-02-27 12:27:51 -0800

    Done with swap chain creation

--- examples/01_init_instance.rs
+++ examples/01_init_instance.rs
@@ -3,6 +3,9 @@ use sarekt::{self, error::SarektError, renderer::VulkanRenderer};
 use std::{error::Error, sync::Arc};
 use winit::{event_loop::EventLoop, window::WindowBuilder};
 
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 800;
+
 struct SarektApp {
   _renderer: VulkanRenderer,
   _event_loop: EventLoop<()>,
@@ -13,7 +16,7 @@ impl SarektApp {
 
     let event_loop = EventLoop::new();
     let window = Arc::new(WindowBuilder::new().build(&event_loop).unwrap());
-    let renderer = VulkanRenderer::new(window.clone()).unwrap();
+    let renderer = VulkanRenderer::new(window.clone(), WIDTH, HEIGHT).unwrap();
 
     Ok(Self {
       _event_loop: event_loop,
